d9ddd17494b5b1b2f18641f8ce7b92b9055b3d53
DanAutomation <kasta-laga@yandex.ru>
2016-08-18 00:30:11 +0300

    updated_18082016

--- Homework/Lesson4/FirstTable/Table1.java
+++ Homework/Lesson4/FirstTable/Table1.java
@@ -1,152 +1,123 @@
 package FirstTable;
 
-
-
 import java.io.FileInputStream;
-//STEP 1. Import required packages
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class Table1 {
 	
-	
- // JDBC driver name and database URL
- static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
- static final String DB_URL = "jdbc:mysql://localhost:3306/test";
-
- //  Database credentials
- static final String USER = "root";
- static final String PASS = "testclub";
- 
-public static void main(String[] args) throws SQLException{
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";   // driver for connection
+	static final String DB_URL = "jdbc:mysql://localhost:3306/test";  // connection to database
+	static final String USER = "root";  // login
+	static final String PASS = "testclub";  // password
+	java.sql.Connection myConn = null;  // create connection
+	String table = "D:/projects/Test/src/FirstTable/createTable"; // link to file, where store data for creation tables
+	String sql1;  // His Majesty sql-request	
 	
 	
-	Table1 a = new Table1();
-	a.getString();
-}
-
-public void getString(){
-    
-	
-	java.sql.Connection myConn = null;
-
-	
-    
-    
-    
-//    a.printRs(rs);
- try{
-
-    Class.forName("com.mysql.jdbc.Driver");
-
-    
-    Properties props = new Properties();
-	props.load(new FileInputStream("D:/projects/Test/src/Lesson4/ini_file"));
-	
-	String theUser = props.getProperty("user");
-	String thePassword = props.getProperty("password");
-	String theDbUrl = props.getProperty("dburl");
-	
-	System.out.println("Connection to database...");
-	System.out.println("Database url: " + theDbUrl);
-	System.out.println("User: " + theUser);
-	
-	myConn = DriverManager.getConnection(theDbUrl, theUser, thePassword);
-	
-	System.out.println("\nConnection successful!\n");
-
-    String table = "D:/projects/Test/src/FirstTable/createTable";  
-	
-    props.load(new FileInputStream(table));
-    
-	String sql = props.getProperty("table");
-	String fillTable = props.getProperty("fillTable");
-	
-	PreparedStatement table1 = (PreparedStatement) myConn.prepareStatement(sql);
-	table1.executeUpdate(sql);
-	
-	PreparedStatement fillTab = (PreparedStatement) myConn.prepareStatement(fillTable);
-	fillTab.executeUpdate(fillTable);
-	
+	public Table1(String sql1){  // constructor
+	       this.sql1 = sql1;	 
+	}
 
 
-	
-	
-	Statement stmt = myConn.createStatement();    
-	
-	
-    String sql1 = "SELECT * FROM Table1";
-	
-    ResultSet rs = stmt.executeQuery(sql1);
-
-
-    printRs(rs);
-
-    //STEP 6: Loop through result set and add 5 in age
-    //Move to BFR postion so while-loop works properly
- //   rs.beforeFirst();
-
-
-
-
-    //STEP 6: Loop through result set and add 5 in age
-    //Move to BFR postion so while-loop works properly
-
-//    printRs(rs);
-
-
-    //STEP 8: Clean-up environment
-  //  rs.close();
-    stmt.close();
-    myConn.close();
- }catch(SQLException se){
-    //Handle errors for JDBC
-    se.printStackTrace();
- }catch(Exception e){
-    //Handle errors for Class.forName
-    e.printStackTrace();
- }finally{
-    //finally block used to close resources
-    try{
-       if(myConn!=null)
-    	   myConn.close();
-    }catch(SQLException se){
-       se.printStackTrace();
-    }//end finally try
- }//end try
-
-
-}//end main
-
- public static void printRs(ResultSet rs) throws SQLException{
-    
-    while(rs.next()){
-    	
-       String a = rs.getString("Field1") + "," + rs.getString("Field2") + "," + rs.getString("Field3");
-    	/*
-       String str1 = rs.getString("Field1");
-       String str2 = rs.getString("Field2");
-       String str3 = rs.getString("Field3");
-       String coma = ",";
-       StringBuilder a = new StringBuilder();
-       a.append(str1).append(coma).append(str2).append(coma).append(str3);
-       StringBuilder sb = new StringBuilder();
-       a.append(","); 
-*/
-       
-       String[] b = a.split("(?=\\p{Lu})"); // split to low-case
-		for (int i = 0; i < b.length; i++){ // every element
-			System.out.print(b[i] + ","); // + space to upper
+		public static void main(String[] args) throws SQLException{
+			
+			
+        String sql1 = "SELECT * FROM Table1";  
+		Table1 a = new Table1(sql1);  // instance of class
+		a.getString();  // call the method of getString()
+		
 		}
+		
+		
+		public void getString(){ // run method getString 
+			
+		try{
+			
+		    Properties props = new Properties();  // create instance of class and input in var props of class Properties
+		props.load(new FileInputStream("D:/projects/Test/src/Lesson4/ini_file"));  // load ini_file
 
+		String theUser = props.getProperty("user");  // find in ini field user, for load file
+		String thePassword = props.getProperty("password");  // find in ini field password, for load file
+		String theDbUrl = props.getProperty("dburl");  // find in ini field dburl, for load link to file
 
-     //выводим результат
-//       System.out.print(a);
+		System.out.println("Connection to database...");  // information message
+		System.out.println("Database url: " + theDbUrl);  // information message
+		System.out.println("User: " + theUser);  // information message
 
-       // .replaceAll("[\\s]", "")
-       
- }//end printRs()
-}//end JDBCExample  
-}
+		myConn = DriverManager.getConnection(theDbUrl, theUser, thePassword);  // connection to db, having information of link. login. password
 
+		System.out.println("\nConnection successful!\n"); // information field
+		     
 
+		
+		props.load(new FileInputStream(table));    // load var with way for creation table
+		String sql = props.getProperty("table");  // find in file var table with script for creation table
+		String fillTable = props.getProperty("fillTable"); // fill table, using var with way
+
+		// execute scripts
+		PreparedStatement table1 = (PreparedStatement) myConn.prepareStatement(sql);  
+		table1.executeUpdate(sql);
+		PreparedStatement fillTab = (PreparedStatement) myConn.prepareStatement(fillTable);
+		fillTab.executeUpdate(fillTable);
+
+  
+		Statement stmt = myConn.createStatement();    // create connection
+	   
+
+		    
+
+		ResultSet rs = stmt.executeQuery(sql1);  // execute query
+		rs.beforeFirst();  // method set cursor to before first record
+//		ResultSetMetaData rsmd = rs.getMetaData();  // create result 
+		printRs(rs);  // input result in method printRs()
+		
+
+		    stmt.close();   // close process
+		    myConn.close();  // close connection
+		}catch(SQLException se){
+		    //Handle errors for JDBC
+		    se.printStackTrace();
+		}catch(Exception e){
+		    //Handle errors for Class.forName
+		    e.printStackTrace();
+		}finally{
+		    //finally block used to close resources
+		    try{
+		       if(myConn!=null)
+		       myConn.close();
+		    }catch(SQLException se){
+		       se.printStackTrace();
+		    }//end finally try
+		}//end try
+	}//end main
+		
+		
+		public static void printRs(ResultSet rs) throws SQLException{    // run method printRs
+			
+		    while(rs.next()){  // while cycle, check every value till end
+
+		    	ArrayList<String> arr = new ArrayList<String>();  // arraylist
+
+		    	
+		    	for(int i = 2; i < 4; i++){ // cycle since 2 column until last
+		    		 
+		    		 arr.add(rs.getString(i) + ",");  // filling arraylist
+
+		    	}
+		    	
+		    	// output into console (not requirement)
+	    		 for(String a : arr){  
+	    	            System.out.print(a); 
+	    	        }
+	
+		       
+   }//end printRs()
+  }//end JDBCExample  
+ }
